94df71829ed9be2ed038a6491068c3648935c434
foutzjesse <foutzjesse@gmail.com>
2022-12-10 12:34:34 -0500

    add more classes

--- scripts/classes.js
+++ scripts/classes.js
@@ -1,22 +1,33 @@
+class Player {
+  constructor(id, name, number, gradclass, positions) {
+    this.id = id;
+    this.name = name;
+    this.number = number;
+    this.gradclass = gradclass;
+    this.positions = positions;
+  }
+}
+
 class OPlay {
   constructor(quarter, lead, clock, down, ballon, togo, formation, play, gain, result, carrier) {
     this.quarter = quarter;
     this.lead = lead; //difference between scores. can be negative.
     this.clock = clock;
     this.down = down;
-    this.ballon = ballon;
+    this.ballon = ballon; //distinguish between own and opponent's
     this.togo = togo;
     this.formation = formation;
     this.play = play;
     this.gain = gain; //can be negative.
     this.result = result;
-    this.carrier = carrier;
+    this.carrier = carrier; //Player
   }
 }
 
 class Game {
-  constructor(date, oplays) {
+  constructor(date, result, oplays) {
     this.date = date;
+    this.result = result;
     this.oplays = oplays;
   }
 }
@@ -25,5 +36,12 @@ class Year {
   constructor(year, games) {
     this.year = year;
     this.games = games;
+  }
+}
+
+class Team {
+  constructor(coach, years){
+    this.coach = coach;
+    this.years = years;
   }
 }
